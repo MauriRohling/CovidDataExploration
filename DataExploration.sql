e1b0395fbacba049f6d16c3fcf190bff8d44b25a
Mauricio Rohling <mauricio.uto@hotmail.com>
2023-08-06 14:36:03 -0300

    update readme add data exploration queries

--- DataExploration.sql
+++ DataExploration.sql
@@ -1,21 +1,73 @@
--- Highest death rate on total population
-SELECT location AS 'Location', MAX(total_cases) AS 'Total Cases', population AS 'Population', (MAX(total_cases) * 1.0 /MAX(population))*100 AS 'Death %'
+---------- DEATH DATA ---------- 
+
+---------- BY COUNTRIES ----------
+
+-- Country with highest infection rate over population
+SELECT location AS Location, (MAX(total_cases) * 1.0 /MAX(population))*100 AS InfectionPercentage, MAX(total_cases) AS TotalCases,  MAX(population) AS Population
 FROM CovidDeaths cd
 WHERE continent IS NOT NULL
-GROUP BY location, population
-ORDER BY 2 DESC
+GROUP BY location
+ORDER BY InfectionPercentage DESC
 
--- Highest death rate on infections
-SELECT location AS Location, (MAX(total_deaths) * 1.0 /MAX(total_cases))*100 AS 'Death %', MAX(total_cases) AS 'Total Cases', MAX(total_deaths) AS 'Total Deaths'
+-- Country with highest death rate over infections
+SELECT location AS Location, (MAX(total_deaths) * 1.0 /MAX(total_cases))*100 AS DeathPercentage, MAX(total_cases) AS TotalCases, MAX(total_deaths) AS TotalDeaths
 FROM CovidDeaths cd
 WHERE continent IS NOT NULL
 GROUP BY location
 HAVING MAX(total_cases) > 10 -- ignore irrelevant numbers
 ORDER BY 2 DESC
 
--- Highest infection rate on population
-SELECT location AS Location, (MAX(total_cases) * 1.0 /MAX(population))*100 AS 'Infection %', MAX(total_cases) AS 'Total Cases',  MAX(population) AS Population
+-- Country with highest death rate over total population
+SELECT location, (MAX(total_deaths) * 1.0 /MAX(population))*100 AS DeathPercentage, MAX(total_cases) AS TotalCases, population
+FROM CovidDeaths cd
+WHERE continent IS NOT NULL
+GROUP BY location, population
+ORDER BY 2 DESC
+
+-- Country with most deaths
+SELECT location, MAX(total_deaths) TotalDeaths
 FROM CovidDeaths cd
 WHERE continent IS NOT NULL
 GROUP BY location
-ORDER BY 'Infection %' DESC
+ORDER BY 2 DESC
+
+
+
+---------- BY CONTINENTS ----------
+
+-- Continent with highest infection rate over population
+SELECT location AS Location, (MAX(total_cases) * 1.0 /MAX(population))*100 AS InfectionPercentage, MAX(total_cases) AS TotalCases,  MAX(population) AS Population
+FROM CovidDeaths cd
+WHERE continent IS NULL
+GROUP BY location
+ORDER BY InfectionPercentage DESC
+
+-- Continent with highest death rate over infections
+SELECT location AS Location, (MAX(total_deaths) * 1.0 /MAX(total_cases))*100 AS DeathPercentage, MAX(total_cases) AS TotalCases, MAX(total_deaths) AS TotalDeaths
+FROM CovidDeaths cd
+WHERE continent IS NULL
+GROUP BY location
+HAVING MAX(total_cases) > 10 -- ignore irrelevant numbers
+ORDER BY 2 DESC
+
+-- Continent with highest death rate over total population
+SELECT location, (MAX(total_deaths) * 1.0 /MAX(population))*100 AS DeathPercentage, MAX(total_deaths) AS TotalDeaths, population
+FROM CovidDeaths cd
+WHERE continent IS NULL
+GROUP BY location, population
+ORDER BY 2 DESC
+
+-- Continent with most deaths
+SELECT location, MAX(total_deaths) TotalDeaths
+FROM CovidDeaths cd
+WHERE continent IS NULL
+GROUP BY location
+ORDER BY 2 DESC
+
+---------- VACCINATION DATA ---------- 
+
+SELECT dth.continent, dth.location, dth.total_cases, vac.new_vaccinations
+FROM CovidDeaths dth
+JOIN CovidVaccinations vac
+	ON dth.location = vac.location
+	AND dth.date = vac.date
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CovidDataExploration Project
 ### How to Run this project
-    Run the PopulateDatabase.bat file to create and populate the tables, it uses SQLEXPRESS and the master by default
+    Run the PopulateDatabase.bat file to create and populate the tables, it uses SQLEXPRESS and the master DB by default
     Otherwise, change the PopulateDatabase.bat file to reflect your environment or run the PopulateDatabase.sql file directly
